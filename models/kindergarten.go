3bea7f7f004eec0de7e03cdd3c7399d9e7297216
sunwen <1451024003@qq.com>
2018-06-15 18:02:00 +0800

    设置园长，修改幼儿园bug,教师学生列表

--- models/kindergarten.go
+++ models/kindergarten.go
@@ -1,7 +1,8 @@
 package models
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
 	"math"
 	"time"
 
@@ -67,10 +68,104 @@ func GetKindergartenById(id int, page, prepage int) map[string]interface{} {
 oms-设置园长
 */
 func AddPrincipal(user_id int, kindergarten_id int, role int) error {
+	o := orm.NewOrm()
+	o.Begin()
+	var or []orm.Params
+	var t []orm.Params
 	var User *UserService
 	client := rpc.NewHTTPClient(beego.AppConfig.String("ONE_MORE_USER_SERVER"))
 	client.UseService(&User)
+	qb, _ := orm.NewQueryBuilder("mysql")
+	sql := qb.Select("*").From("organizational").Where("kindergarten_id = ?").And("type = 1").And("is_fixed = 1").String()
+	_, err := o.Raw(sql, kindergarten_id).Values(&or)
+	if err == nil {
+		if or == nil {
+			err = errors.New("幼儿园没有组织")
+			return err
+		}
+	}
 	uk, _ := User.GetUKByUserId(user_id)
-	fmt.Println(uk)
-	return nil
+	if uk == nil {
+		User.CreateUK(user_id, kindergarten_id, role)
+	} else {
+		uks, _ := json.Marshal(uk)
+		var ukss map[string]interface{}
+		json.Unmarshal(uks, &ukss)
+		uk_id := ukss["id"].(float64)
+		ukId := int(uk_id)
+		err := User.UpdateByUkId(ukId, user_id, kindergarten_id, role)
+		if err != nil {
+			err := errors.New("用户不存在")
+			return err
+		}
+	}
+	if role == 1 || role == 5 || role == 6 {
+		userInfo, err := User.GetOneByUserId(user_id)
+		if err != nil {
+			return err
+		}
+		userinfo, _ := json.Marshal(userInfo)
+		var user map[string]interface{}
+		json.Unmarshal(userinfo, &user)
+		_, err = o.QueryTable("student").Filter("user_id", user_id).Filter("status", 0).Update(orm.Params{
+			"status": 1,
+		})
+		if err != nil {
+			o.Rollback()
+			return err
+		}
+		if err == nil {
+			o.QueryTable("teacher").Filter("user_id", user_id).Filter("status", 0).Update(orm.Params{
+				"status": 1,
+			})
+			if err != nil {
+				o.Rollback()
+				return err
+			}
+		}
+		if role == 1 {
+			qb, _ := orm.NewQueryBuilder("mysql")
+			sql := qb.Select("t.teacher_id").From("teacher as t").Where("user_id = ?").And("status = 0").And("kindergarten_id = ?").String()
+			_, err := o.Raw(sql, user_id, kindergarten_id).Values(&t)
+			if err == nil {
+				if t == nil {
+					sql := "insert into teacher set user_id = ?,name = ?,phone = ?,sex = ?,age = ?,address = ?,kindergarten_id = ?,avatar = ?"
+					id, err := o.Raw(sql, user_id, user["name"].(string), user["phone"].(string), user["sex"], user["age"], user["address"].(string), kindergarten_id, user["avatar"].(string)).Exec()
+					if err != nil {
+						o.Rollback()
+						return err
+					}
+					if err == nil {
+						teacherId, _ := id.LastInsertId()
+						for _, value := range or {
+							sql := "insert into organizational_member set organizational_id = ?,member_id = ?,is_principal = ?,type = ?"
+							o.Raw(sql, value["id"], teacherId, 1, 0).Exec()
+						}
+					} else {
+						o.Rollback()
+						return err
+					}
+				}
+			}
+		} else if role == 5 {
+			sql := "insert into teacher set user_id = ?,name = ?,phone = ?,sex = ?,age = ?,address = ?,kindergarten_id = ?,avatar = ?"
+			_, err := o.Raw(sql, user_id, user["name"].(string), user["phone"].(string), user["sex"], user["age"], user["address"].(string), kindergarten_id, user["avatar"].(string)).Exec()
+			if err != nil {
+				o.Rollback()
+				return err
+			}
+		} else {
+			sql := "insert into student set user_id = ?,name = ?,phone = ?,sex = ?,age = ?,address = ?,kindergarten_id = ?,avatar = ?"
+			_, err := o.Raw(sql, user_id, user["name"].(string), user["phone"].(string), user["sex"], user["age"], user["address"].(string), kindergarten_id, user["avatar"].(string)).Exec()
+			if err != nil {
+				o.Rollback()
+				return err
+			}
+		}
+	}
+	if err == nil {
+		o.Commit()
+		return nil
+	}
+	return err
 }
