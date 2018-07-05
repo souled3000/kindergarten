dc099aa56712978b7625de9b4d88eb96be44fede
yuewei <3215790191@qq.com>
2018-07-05 11:37:36 +0800

    体检详情

--- models/healthy/body.go
+++ models/healthy/body.go
@@ -51,27 +51,31 @@ func GetOneBody(id int) (ml map[string]interface{}, err error){
 	var b Body
 	b.Id = id
 	var num Num
-	list := make(map[string]interface{})
+	var list []map[string]interface{}
 	sql := "select count(a.id) as num from healthy_inspect a where a.body_id = "+strconv.Itoa(id)
 	o.Raw(sql).QueryRow(&num)
 	c_num := num.Num
 	list2 := make(map[string]interface{})
+	list3 := make(map[string]interface{})
 	sql = "select count(a.id) as num from healthy_inspect a where a.body_id = "+strconv.Itoa(id)+" and weight is not null"
 	o.Raw(sql).QueryRow(&num)
 	fmt.Println(num.Num)
 	list2["bili"] = int(math.Ceil(float64(num.Num)/float64(c_num)*100.0))
 	list2["column"] = "weight"
-	list["体重"] = list2
+	list2["name"] = "体重"
+	list = append(list,list2)
 	sql = "select count(a.id) as num from healthy_inspect a where a.body_id = "+strconv.Itoa(id)+" and height is not null"
 	o.Raw(sql).QueryRow(&num)
 	fmt.Println(num.Num)
-	list2["bili"] = int(math.Ceil(float64(num.Num)/float64(c_num)*100.0))
-	list2["column"] = "height"
-	list["身高"] = list2
+	list3["bili"] = int(math.Ceil(float64(num.Num)/float64(c_num)*100.0))
+	list3["column"] = "height"
+	list3["name"] = "身高"
+	list = append(list,list3)
 	if err := o.Read(&b); err == nil {
-
+		var c string
 		project := strings.Split(b.Project,",")
 		for _,val := range project {
+
 			list1 := make(map[string]interface{})
 			cloumn := strings.Split(val,":")
 			sql := "select count(b.id) as num from healthy_inspect a left join healthy_column b on a.id= b.inspect_id where a.body_id = "+strconv.Itoa(id)+" and b."+string(cloumn[0])+" is not null"
@@ -79,11 +83,21 @@ func GetOneBody(id int) (ml map[string]interface{}, err error){
 			fmt.Println(num.Num)
 			list1["bili"] = int(math.Ceil(float64(num.Num)/float64(c_num)*100.0))
 			list1["column"] = cloumn[0]
+
 			if strings.Contains(val, "眼") {
-				list["视力"] = list1
+				if c == "" {
+					c =  cloumn[0]
+				}else {
+					list1["columnx"] = c
+					list1["name"] = "视力"
+					list = append(list,list1)
+				}
+
 			} else {
-				list[string(cloumn[1])] = list1
+				list1["name"] = string(cloumn[1])
+				list = append(list,list1)
 			}
+
 		}
 		bjson,_ :=json.Marshal(b)
 		json.Unmarshal(bjson, &ml)
